1d596e610454c025dc4354c040b41d50da89d2b2
Kevin McAleer <kevin.mcaleer@advicefactory.co.uk>
2016-03-20 18:16:41 +0000

    Initial Commit

---
+++ MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  Principle
+//
+//  Created by Kevin McAleer on 27/10/2009.
+//  Copyright Stoneage Software Ltd 2009 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
